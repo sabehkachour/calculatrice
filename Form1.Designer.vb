1a8e9f82a58d266b3de6fe989e23f481d303c3f0
sabehkachour <sabeh.kackour@isgb.ucar.tn>
2025-01-15 19:29:57 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Buttonpluss = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Buttonmins = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Buttonmult = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Buttonegale = New Button()
+        Buttondiv = New Button()
+        Buttonclear = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.RosyBrown
+        TextBox1.Location = New Point(175, 60)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(394, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Arial", 12F)
+        Button1.Location = New Point(175, 99)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Arial", 12F)
+        Button2.Location = New Point(275, 99)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Arial", 12F)
+        Button3.Location = New Point(375, 99)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonpluss
+        ' 
+        Buttonpluss.Font = New Font("Arial", 12F)
+        Buttonpluss.Location = New Point(475, 99)
+        Buttonpluss.Name = "Buttonpluss"
+        Buttonpluss.Size = New Size(94, 29)
+        Buttonpluss.TabIndex = 4
+        Buttonpluss.Text = "+"
+        Buttonpluss.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Arial", 12F)
+        Button5.Location = New Point(175, 145)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 5
+        Button5.Text = "4"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Arial", 12F)
+        Button6.Location = New Point(275, 145)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 29)
+        Button6.TabIndex = 6
+        Button6.Text = "5"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Arial", 12F)
+        Button7.Location = New Point(375, 145)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 29)
+        Button7.TabIndex = 7
+        Button7.Text = "6"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonmins
+        ' 
+        Buttonmins.Font = New Font("Arial", 12F)
+        Buttonmins.Location = New Point(475, 145)
+        Buttonmins.Name = "Buttonmins"
+        Buttonmins.Size = New Size(94, 29)
+        Buttonmins.TabIndex = 8
+        Buttonmins.Text = "-"
+        Buttonmins.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Arial", 12F)
+        Button9.Location = New Point(175, 193)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 29)
+        Button9.TabIndex = 9
+        Button9.Text = "7"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Arial", 12F)
+        Button10.Location = New Point(275, 193)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 29)
+        Button10.TabIndex = 10
+        Button10.Text = "8"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Arial", 12F)
+        Button11.Location = New Point(375, 193)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 29)
+        Button11.TabIndex = 11
+        Button11.Text = "9"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonmult
+        ' 
+        Buttonmult.Font = New Font("Arial", 12F)
+        Buttonmult.Location = New Point(476, 239)
+        Buttonmult.Name = "Buttonmult"
+        Buttonmult.Size = New Size(94, 29)
+        Buttonmult.TabIndex = 12
+        Buttonmult.Text = "*"
+        Buttonmult.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Arial", 12F)
+        Button13.Location = New Point(175, 240)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(94, 29)
+        Button13.TabIndex = 13
+        Button13.Text = "0"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Font = New Font("Arial", 12F)
+        Button14.Location = New Point(275, 240)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(94, 29)
+        Button14.TabIndex = 14
+        Button14.Text = "c"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonegale
+        ' 
+        Buttonegale.Font = New Font("Yu Gothic Medium", 12F)
+        Buttonegale.Location = New Point(576, 239)
+        Buttonegale.Name = "Buttonegale"
+        Buttonegale.Size = New Size(94, 29)
+        Buttonegale.TabIndex = 15
+        Buttonegale.Text = "="
+        Buttonegale.UseVisualStyleBackColor = True
+        ' 
+        ' Buttondiv
+        ' 
+        Buttondiv.Location = New Point(375, 240)
+        Buttondiv.Name = "Buttondiv"
+        Buttondiv.Size = New Size(94, 29)
+        Buttondiv.TabIndex = 16
+        Buttondiv.Text = "/"
+        Buttondiv.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonclear
+        ' 
+        Buttonclear.Font = New Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Buttonclear.Location = New Point(476, 193)
+        Buttonclear.Name = "Buttonclear"
+        Buttonclear.Size = New Size(94, 29)
+        Buttonclear.TabIndex = 17
+        Buttonclear.Text = "c"
+        Buttonclear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Buttonclear)
+        Controls.Add(Buttondiv)
+        Controls.Add(Buttonegale)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Buttonmult)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Buttonmins)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Buttonpluss)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Buttonpluss As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Buttonmins As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Buttonmult As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Buttonegale As Button
+    Friend WithEvents Buttondiv As Button
+    Friend WithEvents Buttonclear As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class Form1.vb
+    Dim A As String
+    Dim B As String
+    Dim op = ""
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Buttonpluss_Click(sender As Object, e As EventArgs) Handles Buttonpluss.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Buttonmins_Click(sender As Object, e As EventArgs) Handles Buttonmins.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Buttonmult_Click(sender As Object, e As EventArgs) Handles Buttonmult.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Buttondiv_Click(sender As Object, e As EventArgs) Handles Buttondiv.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        TextBox1.Text = A + " " + op + " " + B
+    End Sub
+
+
+    Private Sub Buttonegale_Click(sender As Object, e As EventArgs) Handles Buttonegale.Click
+        Dim Test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" And Not op = "" Then
+                res = va / vb
+            Else
+                MessageBox.Show("Division par 0")
+                Test = False
+            End If
+        Else
+            MessageBox.Show("A et B doivent etre des entiers")
+            Test = False
+        End If
+        If Test Then
+            TextBox1.Text = TextBox1.Text + "=" + res.ToString
+        End If
+
+    End Sub
+
+    Private Sub Buttonclear_Click(sender As Object, e As EventArgs) Handles Buttonclear.Click
+        TextBox1.Text = " "
+
+    End Sub
+End Class
+
+
